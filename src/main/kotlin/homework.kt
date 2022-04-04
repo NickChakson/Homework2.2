957afa771f6122b645638a7b5ecedf6ffc189d86
Gleb Gorbunov <gglebi01@gmail.com>
2022-04-04 20:59:37 +0500

    Version 1.0

--- src/main/kotlin/homework.kt
+++ src/main/kotlin/homework.kt
@@ -2,9 +2,8 @@ fun main() {
 
     print("Количество лайков: ")
     val likes: UInt = readln().toUInt()
-    val solution: String
 
-    solution = if (likes % 100u == 11u) {
+    val solution: String = if (likes % 100u == 11u) {
         "Людям"
     } else if (likes % 10u == 1u) {
         "Человеку"
